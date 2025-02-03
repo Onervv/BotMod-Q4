330733cac6eb2504f415f54c208d371e99f7e85a
Onervv <cperrette123@gmail.com>
2025-02-03 16:21:26 -0500

    def files modified, rocket launcher rapid fire with spread

--- game/weapon/WeaponRocketLauncher.cpp
+++ game/weapon/WeaponRocketLauncher.cpp
@@ -445,11 +445,12 @@ stateResult_t rvWeaponRocketLauncher::State_Fire ( const stateParms_t& parms ) {
 	};	
 	switch ( parms.stage ) {
 		case STAGE_INIT:
-			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));		
-			Attack ( false, 1, spread, 0, 1.0f );
-			PlayAnim ( ANIMCHANNEL_LEGS, "fire", parms.blendFrames );	
+			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier(PMOD_FIRERATE));
+			Attack(false, 3, 5.0f, 0, 0.30f);
+			PlayAnim(ANIMCHANNEL_LEGS, "fire", parms.blendFrames);
+			
 			return SRESULT_STAGE ( STAGE_WAIT );
-	
+
 		case STAGE_WAIT:			
 			if ( wsfl.attack && gameLocal.time >= nextAttackTime && ( gameLocal.isClient || AmmoInClip ( ) ) && !wsfl.lowerWeapon ) {
 				SetState ( "Fire", 0 );
